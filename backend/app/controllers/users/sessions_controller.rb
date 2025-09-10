0212bb1b71604d9d17a35a0f3f6d9ae33933575f
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-09-10 00:28:13 -0300

    Improve SessionsController code

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -3,8 +3,10 @@ class Users::SessionsController < Devise::SessionsController
 
   def create
     self.resource = warden.authenticate(auth_options)
+
     if resource
       sign_in(resource_name, resource)
+
       render json: { message: 'Logged in', user: resource }, status: :ok
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
@@ -14,6 +16,7 @@ class Users::SessionsController < Devise::SessionsController
   def destroy
     if current_user
       sign_out(resource_name)
+
       render json: { message: 'Logged out' }, status: :ok
     else
       render json: { message: 'No active session' }, status: :unauthorized
