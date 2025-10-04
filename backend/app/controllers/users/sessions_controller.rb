ab2aa16fceeaecfb89d67e18c04f0a8cfcc79a67
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-10-04 02:49:50 -0300

    Add TrackEventJob to SessionsController

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -3,10 +3,15 @@ class Users::SessionsController < Devise::SessionsController
 
   def create
     self.resource = warden.authenticate(auth_options)
-
     if resource
-      sign_in(resource_name, resource)
+      TrackEventJob.perform_later(
+        resource.id,
+        "login",
+        { ip: request.remote_ip, user_agent: request.user_agent },
+        Time.current
+      )
 
+      sign_in(resource_name, resource)
       token = Warden::JWTAuth::UserEncoder.new.call(resource, :user, nil)
       response.set_header('Authorization', "Bearer #{token[0]}")
 
@@ -18,6 +23,12 @@ class Users::SessionsController < Devise::SessionsController
 
   def destroy
     if current_user
+      TrackEventJob.perform_later(
+        current_user.id,
+        "logout",
+        { ip: request.remote_ip },
+        Time.current
+      )
       sign_out(resource_name)
       render json: { message: 'Logged out' }, status: :ok
     else
