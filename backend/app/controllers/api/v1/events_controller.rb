e4a8fb9222f9e8a41170d2ac04bfec1272455d22
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-10-12 22:06:21 -0300

    Refactoring events_controller to show filtered events

--- backend/app/controllers/api/v1/events_controller.rb
+++ backend/app/controllers/api/v1/events_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::EventsController < Api::V1::BaseController
-  def login_stats
-    data = Event.where(user_id: current_user.id, event_type: "login")
+  def event_stats
+    start_date = 30.days.ago.beginning_of_day
+
+    data = Event.where('occurred_at >= ?', start_date)
+                .group(:event_type)
                 .group_by_day(:occurred_at)
                 .count
 
-    render json: data
+    formatted = {}
+    data.each do |(type, date), count|
+      formatted[date] ||= { date: date, login: 0, signup: 0, logout: 0 }
+      formatted[date][type] = count
+    end
+
+    render json: formatted.values.sort_by { |d| d[:date] }
   end
 end
