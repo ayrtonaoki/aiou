6242fee46b5925e5f6a77ef3a9e012d03ea9959d
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-10-12 22:07:02 -0300

    Add User and Events relation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :events, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
